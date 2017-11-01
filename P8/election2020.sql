97df8c4c138fc46e6a005f03d7e17aca6dc71d1d
JonnyKong <jonnykong1996@gmail.com>
2017-11-01 02:11:29 -0400

    updates

--- P8/election2020.sql
+++ P8/election2020.sql
@@ -5,5 +5,5 @@ INSERT INTO election_result VALUES
 (30000, 20000, (SELECT id FROM party WHERE name = 'Liberal Party of Canada'), null),
 (30001, 20000, (SELECT id FROM party WHERE name = 'Green Party of Canada'), 30000),
 (30002, 20000, (SELECT id FROM party WHERE name = 'Rhinoceros Party'), 30000),
-(30003, 20000, (SELECT id FROM party WHERE name = 'Co-operative Commonwealth Federation | New Democratic Party'), null),
+(30003, 20000, (SELECT id FROM party WHERE name = 'National Democratic and Labour Party '), null),
 (30004, 20000, (SELECT id FROM party WHERE name = 'Conservative Party of Canada'), 30003);
