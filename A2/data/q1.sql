b5c7debb8c1634fd8bcaf43866e9151cc5933d0f
JonnyKong <jonnykong1996@gmail.com>
2017-11-01 21:01:07 -0400

    updates

--- A2/data/q1.sql
+++ A2/data/q1.sql
@@ -18,12 +18,24 @@ partyName VARCHAR(100)
 DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
+
+-- If votes_valid field is NULL, fill it with the sum of valid votes of parties
 CREATE VIEW election_full AS
 SELECT id, country_id, e_date,
 	(CASE WHEN votes_valid IS NOT NULL THEN votes_valid
-		 WHEN votes_valid IS NULL THEN (SELECT SUM(votes) FROM election_result where election_result.election_id = election.id)
+		 WHEN votes_valid IS NULL THEN
+			(SELECT SUM(votes) FROM election_result 
+			WHERE election_result.election_id = election.id)
 	END) AS votes_total
 FROM election;
+
+--
+SELECT e_date.year country_id, party_id, SUM(votes) / SUM(votes_total) AS voteRange
+FROM election_result, election_full
+WHERE election_result.election_id = election_full.id
+	AND e_date.year >= 1996 AND e-date.year <= 2016
+GROUP BY e_date.year, country_id, party_id
+HAVING voteRange <= 0.05;
 
 
 
