b80d232f491edc7d7c93e90ca2ec9bf916560f07
JonnyKong <jonnykong1996@gmail.com>
2017-11-01 21:38:37 -0400

    updates

--- A2/data/q1.sql
+++ A2/data/q1.sql
@@ -31,7 +31,7 @@ FROM election;
 
 -- GROUP BY year, country and party
 SELECT EXTRACT(year FROM e_date) AS year, country_id, party_id, 
-	((CASE WHEN SUM(votes) IS NOT NULL THEN SUM(votes) ELSE 0) / SUM(votes_total)) AS vote_range
+	((CASE WHEN SUM(votes) IS NOT NULL THEN SUM(votes) ELSE SUM(votes)) / SUM(votes_total)) AS vote_range
 FROM election_result, election_full
 WHERE election_result.election_id = election_full.id
 	AND e_date >= '1996-01-01' AND e_date <= '2016-12-31'
