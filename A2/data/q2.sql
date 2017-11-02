2c4670b29a1dfde9f389629d66fd51751634bd41
YifanFang <33300072+YifanFang@users.noreply.github.com>
2017-11-02 09:56:50 +0800

    Update q2.sql

--- A2/data/q2.sql
+++ A2/data/q2.sql
@@ -1,3 +1,5 @@
+
+
 -- Winners
 
 SET SEARCH_PATH TO parlgov;
@@ -14,7 +16,6 @@ mostRecentlyWonElectionId INT,
 mostRecentlyWonElectionYear INT
 );
 
-
 -- You may find it convenient to do this for each of the views
 -- that define your intermediate steps.  (But give them better names!)
 DROP VIEW IF EXISTS intermediate_step CASCADE;
@@ -25,4 +26,24 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 -- the answer to the query 
 insert into q2 
 
+
+CREATE winner_vote AS 
+SELECT election_id,max(votes)AS max_vote FROM election_result GROUP BY election_id
+
+CREATE winner AS
+SELECT party_id,country_id FROM election_result NATURAL JOIN winner_vote JOIN party ON party.id = election_result.party_id
+       WHERE winner_vote.max_vote = election_result.votes
+
+CREATE num_win AS
+SELECT party_id, country_id, count(*) AS num_of_winning FROM winner GROUP BY party_id
+
+CREATE country_avg_win AS
+SELECT country_id, sum(num_of_winning)/count(*) AS average 
+       FROM num_win FULL JOIN party ON num_win.party_id = party.party_id GROUP BY country_id
+
+CREATE Answer_party AS
+SELECT party_id, country_id FROM num_win NATURAL JOIN country_avg_win WHERE 3*average < num_of_winning
+
+SELECT country.name, party.name, party_family.family, num_of_winning FROM num_win n, country c, party p, party_family pf
+       WHERE n.party_id = p.id, n.country_id = c.id, n.party_id = pf.party_id
 
