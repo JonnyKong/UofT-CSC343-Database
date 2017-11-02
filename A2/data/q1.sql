220c79a57537a172b962d532c6352e9787a8d618
JonnyKong <jonnykong1996@gmail.com>
2017-11-01 22:05:16 -0400

    updates

--- A2/data/q1.sql
+++ A2/data/q1.sql
@@ -28,14 +28,26 @@ SELECT id, country_id, e_date,
 	END) AS votes_total
 FROM election;
 
--- GROUP BY year, country and party
-CREATE VIEW grouped AS
-SELECT EXTRACT(year FROM e_date) AS year, country_id, party_id, 
-	COALESCE((SUM(votes) / votes_total), 0) AS vote_range
+CREATE VIEW groupedByElection AS
+SELECT e_date, country_id, party_id, COALESCE((SUM(votes) / votes_total), 0) AS vote_range
 FROM election_result, election_full
 WHERE election_result.election_id = election_full.id
 	AND e_date >= '1996-01-01' AND e_date <= '2016-12-31'
+GROUP BY election_full.id, country_id, party_id;
+
+CREATE VIEW grouped AS
+SELECT AVG(vote_range)
+FROM groupedByElection
 GROUP BY EXTRACT(year FROM e_date), country_id, party_id;
+
+-- GROUP BY year, country and party
+-- CREATE VIEW grouped AS
+-- SELECT EXTRACT(year FROM e_date) AS year, country_id, party_id, 
+-- 	COALESCE((SUM(votes) / votes_total), 0) AS vote_range
+-- FROM election_result, election_full
+-- WHERE election_result.election_id = election_full.id
+-- 	AND e_date >= '1996-01-01' AND e_date <= '2016-12-31'
+-- GROUP BY EXTRACT(year FROM e_date), country_id, party_id;
 
 
 -- the answer to the query 
