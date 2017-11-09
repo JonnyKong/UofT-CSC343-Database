334fb370ba2b7244c1721e880dd256029b8e1b84
YifanFang <33300072+YifanFang@users.noreply.github.com>
2017-11-09 12:45:20 +0800

    Update q4.sql

--- A2/data/q4.sql
+++ A2/data/q4.sql
@@ -25,3 +25,35 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 -- the answer to the query 
 INSERT INTO q4 
 
+CREATE number_in_0_2 AS 
+SELECT count(*)AS r0_2, party.country_id
+FROM party,party_position ON party.id = party_position.party_id
+WHERE party_position.left_right < 2
+GROUP BY party.country_id
+
+CREATE number_in_2_4 AS 
+SELECT count(*)AS r2_4, party.country_id
+FROM party,party_position ON party.id = party_position.party_id
+WHERE 2 < party_position.left_right < 4
+GROUP BY party.country_id
+
+CREATE number_in_4_6 AS 
+SELECT count(*)AS r4_6, party.country_id
+FROM party,party_position ON party.id = party_position.party_id
+WHERE 4 < party_position.left_right < 6
+GROUP BY party.country_id
+
+CREATE number_in_6_8 AS 
+SELECT count(*)AS r6_8, party.country_id
+FROM party,party_position ON party.id = party_position.party_id
+WHERE 4 < party_position.left_right < 6
+GROUP BY party.country_id
+
+CREATE number_in_8_10 AS 
+SELECT count(*)AS r8_10, party.country_id
+FROM party,party_position ON party.id = party_position.party_id
+WHERE 8 < party_position.left_right < 10
+GROUP BY party.country_id
+
+SELECT name,r0_2,r2_4,r4_6, r6_8,r8_10
+FROM country NATURAL JOIN number_in_0_2 NATURAL JOIN number_in_2_4 NATURAL JOIN number_in_4_6 NATURAL JOIN number_in_6_8 NATURAL JOIN number_in_8_10
