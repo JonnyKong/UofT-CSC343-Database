4af053780f322f77208eb189aaa67102b3256937
YifanFang <33300072+YifanFang@users.noreply.github.com>
2017-11-12 06:50:39 +0800

    Update q2.sql

--- A2/data/q2.sql
+++ A2/data/q2.sql
@@ -22,20 +22,26 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 
 -- Define views for your intermediate steps here.
 
+--Find the number of vote that win that election for each election
 CREATE winner_vote AS 
 SELECT election_id,max(votes)AS max_vote FROM election_result GROUP BY election_id ;
 
+--Find the party that wins the election for each election
 CREATE winner AS
 SELECT party_id,country_id FROM election_result NATURAL JOIN winner_vote JOIN party ON party.id = election_result.party_id
        WHERE winner_vote.max_vote = election_result.votes ;
 
+--Find the number of win for each party. For the party that does not win, set 0.
 CREATE num_win AS
-SELECT party_id, country_id, count(*) AS num_of_winning FROM winner GROUP BY party_id
+SELECT winner.party_id, count(country_id) AS num_of_winning 
+FROM winner  RIGHT JOIN party ON winner.party_id = party.id GROUP BY party_id
 
+--Find the average number of winning elections of each country
 CREATE country_avg_win AS
-SELECT country_id, sum(num_of_winning)/count(*) AS average 
-       FROM num_win FULL JOIN party ON num_win.party_id = party.party_id GROUP BY country_id ;
+SELECT party.country_id, sum(num_win.num_of_winning)/count(party.party_id) AS average 
+       FROM num_win FULL JOIN party ON num_win.party_id = party.party_id GROUP BY party.country_id ;
 
+--Find the party that that have won three times the average number of winning elections of parties of the same country
 CREATE Answer_party AS
 SELECT party_id, country_id FROM num_win NATURAL JOIN country_avg_win WHERE 3*average < num_of_winning ;
 
