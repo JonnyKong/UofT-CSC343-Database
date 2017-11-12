ffafa92a48a0fc7c7c15bfa2aa8936e888fed55e
YifanFang <33300072+YifanFang@users.noreply.github.com>
2017-11-12 17:30:12 +0800

    Update q6.sql

--- A2/data/q6.sql
+++ A2/data/q6.sql
@@ -22,16 +22,16 @@ DROP VIEW IF EXISTS intermediate_step CASCADE;
 -- Create a table which includes end date of each cabinet(if it is not the most recent)
 CREATE VIEW end_time AS
 SELECT c1.id AS cabinet_id,  c2.start_date AS endDate
-FROM cabinet c1 JOIN cabinet c2 ON c1.id=c2. previous_cabinet_id;
+FROM cabinet c1 JOIN cabinet c2 ON c1.id=c2. previous_cabinet_id AND c1.id > c2.id;
 
 -- Create a table as cabinet but with end date of each cabinet. For the most recent cabinet, set NULL for the endDate.
 CREATE VIEW with_end_time AS
-SELECT * FROM cabinet NATURAL FULL JOIN end_time ;
+SELECT * FROM cabinet LEFT JOIN end_time ON cabinet.id = end_time.cabinet_id;
 
 -- Create a table as with_end_time with the party id of the primary minister party of each cabinet
 CREATE VIEW with_party_prime_minister AS
 SELECT w.country_id, w.id AS cabinetId, w.start_date AS startDate, w.endDate, c.party_id
-FROM with_end_time w JOIN cabinet_party c ON w.cabinet_id = c.cabinet_id WHERE c.pm ;
+FROM with_end_time w LEFT JOIN cabinet_party c ON w.cabinet_id = c.cabinet_id WHERE c.pm ;
 
 -- Create a table as with_party_prime_minister but from country id to country name
 CREATE VIEW with_country_name AS
