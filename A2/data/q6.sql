4223297099f34407b5b37f27acf608d2ebc3c0c4
YifanFang <33300072+YifanFang@users.noreply.github.com>
2017-11-15 11:03:57 +0800

    Update q6.sql

--- A2/data/q6.sql
+++ A2/data/q6.sql
@@ -24,19 +24,26 @@ CREATE VIEW with_end_time AS
 SELECT c1.id AS cabinet_id, c1.country_id, c1.start_date, c2.start_date AS endDate 
 FROM cabinet c1 LEFT JOIN cabinet c2 ON c1.id = c2.previous_cabinet_id;
 
--- Create a table as with_end_time with the party id of the primary minister party of each cabinet
-CREATE VIEW with_party_prime_minister AS
-SELECT w.country_id, w.cabinet_id AS cabinetId, w.start_date AS startDate, w.endDate, c.party_id
+-- Create a table as with_end_time with the party id of the primary minister party of each cabinet except those without prime minister
+CREATE VIEW with_party_prime_minister_not_null AS
+SELECT w.country_id, w.cabinet_id, w.start_date, w.endDate, c.party_id
 FROM with_end_time w LEFT JOIN cabinet_party c ON w.cabinet_id = c.cabinet_id WHERE c.pm ;
 
--- Create a table as with_party_prime_minister but from country id to country name
-CREATE VIEW with_country_name AS
-SELECT c.name AS countryName, w.cabinetId, w.startDate, w.endDate, w.party_id
-FROM with_party_prime_minister w JOIN country c ON w.country_id = c.id ;
+-- Create a table as with_party_prime_minister_not_null with the party name of 
+--the primary minister party of each cabinet except those without prime minister
+CREATE VIEW with_party_name_not_null AS
+SELECT w.country_id, w.cabinet_id AS cabinetId, w.start_date AS startDate, w.endDate, p.name
+FROM with_party_prime_minister_not_null w LEFT JOIN party p ON w.party_id = p.id ;
+
+-- Create a table as with_end_time with the party id of the primary minister party of each cabinet
+CREATE VIEW with_party_prime_minister AS
+SELECT w1.country_id, w1.cabinet_id, w1.start_date, w1.endDate, w2.name
+FROM with_end_time w1 LEFT JOIN with_party_name_not_null w2 ON w1.cabinet_id = w2.cabinetId ;
 
 -- the answer to the query 
--- As woth_country_name but from party id to party name
+--Create a table as with_party_prime_minister but from country id to country name
 insert into q6 
 
-SELECT w.countryName, w.cabinetId, w.startDate, w.endDate,  p.name AS pmParty
-FROM with_country_name w JOIN party p ON w.party_id = p.id ;
+CREATE VIEW with_country_name AS
+SELECT c.name AS countryName, w.cabinet_id, w.start_date, w.endDate, w.party_id
+FROM with_party_prime_minister w JOIN country c ON w.country_id = c.id ;
