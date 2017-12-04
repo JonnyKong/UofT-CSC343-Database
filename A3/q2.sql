14095c88eb717ae68f84e72d877c2c5d0446046c
YifanFang <33300072+YifanFang@users.noreply.github.com>
2017-12-04 09:54:28 +0800

    Update q2.sql

--- A3/q2.sql
+++ A3/q2.sql
@@ -1,3 +1,7 @@
+--Create the final answer in which report the question by its question id with
+--its first 50th character question text and the number of hints of that specific question
+--Note in our schema, we store the hints of multiple choice question and numeric question
+--in two views, that's why we use union in this question
 SELECT question_bank.id, 
 	left(question_bank.questionText, 50),
 	hint_count.count
