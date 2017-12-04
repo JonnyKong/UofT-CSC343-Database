1fcf29632d31e847b4f584a6974aca6c597fa460
YifanFang <33300072+YifanFang@users.noreply.github.com>
2017-12-04 09:56:02 +0800

    Update q1.sql

--- A3/q1.sql
+++ A3/q1.sql
@@ -1,3 +1,5 @@
+--Create the final answer in which report all the student's fullname
+--in the database with their student id
 SELECT (studentFirstName || ' ' || studentLastName) AS fullname, 
 	lpad(id::text, 10, '0') AS student_number
 FROM student;
